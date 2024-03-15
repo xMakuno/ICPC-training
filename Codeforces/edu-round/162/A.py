f53d23d5c432031b670ae656a66987f6c2266410
xMakuno <mamv2001@gmail.com>
2024-03-15 07:03:18 -0600

    🎨update: refactor edu round 162 problem A

--- Codeforces/edu-round/162/A.py
+++ Codeforces/edu-round/162/A.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     n = int(input())
     for i in range(n):
         input()
-        chips = [ int(a) for a in input().split(" ")]
+        chips = [int(a) for a in input().split(" ")]
         firstChip = chips.index(1)
         lastChip = next(i for i in reversed(range(len(chips))) if chips[i] == 1)
         chips = chips[firstChip:lastChip]
