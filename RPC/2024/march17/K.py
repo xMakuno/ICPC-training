fcf0f24387e6745eae694af7e3dd9ec6d6d9c827
xMakuno <mamv2001@gmail.com>
2024-03-19 13:11:36 -0600

    👍add: PS for K  - LATAM Championship 2024

--- RPC/2024/march17/K.py
+++ RPC/2024/march17/K.py
@@ -1,4 +1,40 @@
-vowels = ['A','E','I','O','U','Y']        
+vowels = ['A','E','I','O','U','Y']
+def KMPSearch(pat, txt):
+    M = len(pat)
+    N = len(txt)
+    lps = [0]*M
+    j = 0
+    computeLPSArray(pat, M, lps)
+    i = 0
+    print(lps)
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+        if j == M:
+            print ("Found pattern at index", str(i-j))
+            j = lps[j-1]
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+ 
+def computeLPSArray(pat, M, lps):
+    len = 0
+    lps[0]
+    i = 1
+    while i < M:
+        if pat[i]== pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
 
 def solve():
     n = int(input())
@@ -8,15 +44,18 @@ def solve():
         acronym = ""
         megaList = []
         for i in range(n):
-            temp = ['1' if x in vowels else '0' for x in [*input()]]
+            temp = ['V' if x in vowels else 'C' for x in [*input()]]
+            if len(temp) >= 3:
+                temp = temp[:3]
+            print(f'Temp: {temp}')
             megaList.append(temp)
             acronym += temp[0]
-        print(megaList)
-        print(acronym)
+        KMPSearch("CCC", acronym)
+        
+        
 
 if __name__ == "__main__":
-    # solve()
-    print(1)
+    solve()
 
 '''
 3
