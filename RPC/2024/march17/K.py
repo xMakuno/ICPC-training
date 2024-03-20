60790db7362f0f7bf983ff1e61f91485dbe625fa
xMakuno <mamv2001@gmail.com>
2024-03-20 08:41:15 -0600

    👍add: partial solution problem K ICPC March 17

--- RPC/2024/march17/K.py
+++ RPC/2024/march17/K.py
@@ -1,41 +1,24 @@
 vowels = ['A','E','I','O','U','Y']
-def KMPSearch(pat, txt):
-    M = len(pat)
+def KMPSearch(txt):
+    pat = "CCC"
+    M = 3
     N = len(txt)
-    lps = [0]*M
+    lps = [0,1,2]
     j = 0
-    computeLPSArray(pat, M, lps)
     i = 0
-    print(lps)
+    unp = []
     while i < N:
         if pat[j] == txt[i]:
             i += 1
             j += 1
         if j == M:
-            print ("Found pattern at index", str(i-j))
+            unp.append(j-2)
             j = lps[j-1]
         elif i < N and pat[j] != txt[i]:
             if j != 0:
                 j = lps[j-1]
             else:
                 i += 1
- 
-def computeLPSArray(pat, M, lps):
-    len = 0
-    lps[0]
-    i = 1
-    while i < M:
-        if pat[i]== pat[len]:
-            len += 1
-            lps[i] = len
-            i += 1
-        else:
-            if len != 0:
-                len = lps[len-1]
-            else:
-                lps[i] = 0
-                i += 1
-
 def solve():
     n = int(input())
     if n <= 2:
@@ -50,7 +33,7 @@ def solve():
             print(f'Temp: {temp}')
             megaList.append(temp)
             acronym += temp[0]
-        KMPSearch("CCC", acronym)
+        KMPSearch(acronym)
         
         
 
