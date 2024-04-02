21c1fa55ae81efd182fcef69cadd7a71be4a4734
xMakuno <mamv2001@gmail.com>
2024-04-01 23:33:02 -0600

    👍add: c++ template for cp

---
+++ algorithms/template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(n) for(int i=0;i<n;i++)
+#define foi(m,n) for(int i=m;i<n;i++)
+#define foj(m,n) for(int j=m;j<n;j++)
+#define rev(n) for(int i=n-1;i>=0;i--)
+#define revj(n) for(int j=n-1;j>=0;j--)
+#define mp make_pair
+#define fi first
+#define sec second
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define endl '\n'
+#define printa(v) for(auto x: v) { cout << x << " ";}
+
+signed main(){
+    return 0;
+}
