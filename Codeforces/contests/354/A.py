88e735b3fac5e0b76f64c1df138d0ee94d944976
xMakuno <mamv2001@gmail.com>
2024-04-05 11:42:28 -0600

    👍add: problem A AC contest 354

---
+++ Codeforces/contests/354/A.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    n = int(input())
+    indexes = []
+    for i, num in enumerate(input().split()):
+        num = int(num)
+        if num == 1  or num == n:
+            indexes.append(i)
+    print(max(n - 1 - indexes[1], indexes[0] - 0) + indexes[1] - indexes[0])
