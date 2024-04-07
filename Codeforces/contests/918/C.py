139bb428767a9972162918da86ca5b60d0240de4
xMakuno <mamv2001@gmail.com>
2024-04-06 20:26:53 -0600

    👍add: AC for problem C contest 918

--- Codeforces/contests/918/C.py
+++ Codeforces/contests/918/C.py
@@ -7,7 +7,9 @@ Topic: binary search, implementation
 Elo: 800
 '''
 import math
-def implementation(): #AC for problem 296ms
+def implementation(): 
+    # AC for problem 296ms in PyPy due to math library
+    #                171ms in CPython due to math library
     input()
     sum = 0
     for n in input().split():
@@ -17,8 +19,24 @@ def implementation(): #AC for problem 296ms
     else:
         print('NO')
 
+def bin_search():
+    input()
+    sum = 0
+    for n in input().split():
+        sum += int(n)
+    l = 1
+    r = 1e9
+    while( l <= r):
+        mid = l + (r - l)//2
+        if mid*mid == sum: return "YES"
+        if mid*mid > sum: r = mid -1
+        else: l =  mid + 1
+    return "NO"
+
 if __name__ == "__main__":
     t = int(input())
     while t:
         t-=1
-        implementation()
+        # implementation()
+        #hehe
+        print(bin_search())
