c0a6a25ec96db143fa44d0b3070e86ec72c0f27c
xMakuno <mamv2001@gmail.com>
2024-04-08 20:51:20 -0600

    👍add: correct previous code which is WA to AC

--- Codeforces/contests/756/B.py
+++ Codeforces/contests/756/B.py
@@ -6,10 +6,9 @@ Problem: B
 Topic: constructive algorithms, math
 Elo: 800
 '''
-import math
 def solve():
     a, b = list(map(int, input().split()))
-    return min(a,min(b, (a+b)//2))
+    return min(a, b, (a+b) >> 2)
 
 if __name__ == "__main__":
     t = int(input())
