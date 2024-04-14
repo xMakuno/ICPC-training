8832d2a5dac277bf3bb1fdd756bd5b7d9f396677
xMakuno <mamv2001@gmail.com>
2024-04-13 22:38:42 -0600

    👍add: AC for a in edu 163

--- Codeforces/edu-round/163/A.py
+++ Codeforces/edu-round/163/A.py
@@ -1,10 +1,17 @@
-def solve():
+def WA():
     n, m, k = map(int, input().split())
     if n == 1:
         print("NO")
     elif m <= k:
         print("NO")
     elif n <= k:
+        print("NO")
+    else:
+        print("YES")
+def solve():
+    n, m, k = map(int, input().split())
+    z = n // m + (0 if n % m ==0 else 1)
+    if k >= n - z:
         print("NO")
     else:
         print("YES")
