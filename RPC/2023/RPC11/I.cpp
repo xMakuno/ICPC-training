8e55eecab6f7e2e0856a838262ef878126de558a
xMakuno <mamv2001@gmail.com>
2024-04-27 19:08:00 -0600

    👍remove: code from I ICPC august 26

--- RPC/2023/RPC11/I.cpp
+++ RPC/2023/RPC11/I.cpp
@@ -2,61 +2,24 @@
 using namespace std;
 
 void solve(){
-    int n, ans = 0;
-    int temp;
-    bool flipped = false;
-    string memo = "";
+    int n, ans = 0, t;
+    int a[2] = {0,0};
     cin >> n;
     for(int i = 0; i < n; ++i){
-        cin >> temp;
-        if(i == 0){
-            if(temp == 0){
-                memo += to_string(temp);
-                // cout << "Memo " << memo << " Answer " << ans << endl;
-            }else{
-                ++ans;
-                memo += to_string(temp);
-                //cout << "Memo " << memo << " Answer " << ans << endl; 
-            }
-            cout << "Memo " << memo << " Answer " << ans << endl;
-            continue;
-        }
-
-        if(temp == 0){
-            // cout << "Memo " << memo << " Answer " << ans << endl;
-            memo += to_string(temp);
-            ans +=  !flipped ? __builtin_popcount(stoi(memo, nullptr, 2)) : memo.size() - __builtin_popcount(stoi(memo, nullptr, 2));
+        cin >> t;
+        if( t == 1){
+            swap(a[0],a[1]);
+            a[1]++;
+            ans += a[1];
         }else{
-            ++ans;
-            flipped = !flipped;
-            ans +=  !flipped ? __builtin_popcount(stoi(memo, nullptr, 2)) : memo.size() - __builtin_popcount(stoi(memo, nullptr, 2));
-            // cout << "Memo " << memo << " Answer " << ans << endl;
-            memo += to_string(temp);
-        }
-        cout << "i " << i << "Memo " << memo << " Answer " << ans << endl;
-    }
-    cout << ans << endl;
-}
-
-void solve2(){
-    int n, ans = 0, a[2]= {0,0}, k =0, w=0;
-    cin >> n;
-    vector<int> v(n);
-    for(int i = 0; i < n; ++i){
-        cin >> v[i];
-        ++a[k%2];
-        if(v[i]==1){
-            ans += a[k%2];
-            w = a[k%2];
-            k++;
-        }else{
-            ans += w;
+            a[0]++;
+            ans += a[1];
         }
     }
-    cout << ans;
+    cout << ans << '\n';
 }
 
 int main(){
-    solve2();
+    solve();
     return 0;
 }
