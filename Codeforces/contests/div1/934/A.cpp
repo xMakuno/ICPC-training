e5abff4d5a66e278007762561874fe06085b3663
xMakuno <mamv2001@gmail.com>
2024-04-27 19:09:40 -0600

    👍add: partial code for A round 934 div 1

---
+++ Codeforces/contests/div1/934/A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(n) for(int i=0;i<n;i++)
+#define foi(m,n) for(int i=m;i<n;i++)
+#define foj(m,n) for(int j=m;j<n;j++)
+#define rev(n) for(int i=n-1;i>=0;i--)
+#define revj(n) for(int j=n-1;j>=0;j--)
+#define mp make_pair
+#define fi first
+#define sec second
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define endl '\n'
+#define printa(v) for(auto x: v) { cout << x << " ";}
+
+void solve(){
+    int n, mex=0, t;
+    cin >> n;
+    int arr[n];
+    
+
+    /* int array[2e5];
+    fo(2e5) array[i] = 0;
+    fo(n){
+        cin >> t;
+        array[t]++;
+    } */
+}
+
+signed main(){
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
