812ad99d5461b4238ce3c044e1e25869363f4b65
xMakuno <mamv2001@gmail.com>
2024-04-28 20:54:40 -0600

    👍add: TLE for E atcoder april 28

--- Atcoder/abril17/E.py
+++ Atcoder/abril17/E.py
@@ -1,10 +1,22 @@
 if __name__ == "__main__":
     n,x,m = map(int, input().split())
-    pre = []
-    cycle = {}
+    pre = set({})
+    cycle = set({})
+    i=0
     while True:
-        rem = x %m
-        if x < m: # Not in the cycle
-            pre.append(rem)
+        rem = x % m
+        if rem not in pre: pre.add(rem)
         else:
-            cycle.append(rem)
+            if rem not in cycle:
+                cycle.add(rem)
+            else:
+                break
+        print(rem)
+        x = x**2
+    cyclic_remainders = n - len(pre)
+    ans = 0
+    leftovers = cyclic_remainders % len(cycle)
+    ans += sum(pre)
+    ans += sum(cycle)*cyclic_remainders//len(cycle)        
+    for i in range(leftovers): ans += cycle[i]
+    print(ans)
