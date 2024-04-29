8f0566b3e63c1eb8aa3dc029eef641a3a1402210
xMakuno <mamv2001@gmail.com>
2024-04-29 09:49:37 -0600

    👍add: solution for problem D round 937 div4

--- Codeforces/contests/div4/937/D.py
+++ Codeforces/contests/div4/937/D.py
@@ -4,16 +4,27 @@ Contest: Codeforces Round 937 Div 4
 Link: https://codeforces.com/contest/1950
 Problem: D
 Topic: brute force, dp, implementation, number theory
-Elo: 800
+Elo: 1100
 '''
-if __name__ == "__main__": #WA bad implementation
+if __name__ == "__main__": # solution in python
+    numbas = []
+    for i in range(2,100_000+1):
+        c = i
+        flag = False
+        while c > 0:
+            if(c%10>1):
+                flag = True
+            c = c//10
+        if not flag: numbas.append(i)
+    # t = 1
     t = int(input())
     while t:
         t-=1
-        num = set(input())
-        if '1' in num and '0' in num and len(num) == 2:
-            print("YES")
-        elif len(num) == 1 and ('1' in num or '0' in num ):
+        n = int(input())
+        for k in range(len(numbas)-1,-1,-1):
+            while n % numbas[k] == 0:
+                n = n // numbas[k]
+        if n == 1:
             print("YES")
         else:
             print("NO")
