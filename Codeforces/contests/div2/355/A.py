a5f86eea32965bb3d6c0000a0981ebf721125ea0
xMakuno <mamv2001@gmail.com>
2024-04-29 21:34:46 -0600

    👍refactor file to correspond to its contest and div

--- Codeforces/contests/div2/355/A.py
+++ Codeforces/contests/div2/355/A.py
@@ -1,3 +1,11 @@
+'''
+Site: Codeforces
+Contest: Codeforces Round 355 Div 2
+Link: https://codeforces.com/contest/677
+Problem: A
+Topic: implementation, math
+Elo: 800
+'''
 if __name__ == "__main__":
     n, h = [int(x) for x in input().split(" ")]
 
