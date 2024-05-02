e5ad4290c333577766ec058230cf4bc57e03fb5c
xMakuno <mamv2001@gmail.com>
2024-05-02 09:55:18 -0600

    👍add: UV sol for A round 943 div3

--- Codeforces/edu-round/165/B.py
+++ Codeforces/edu-round/165/B.py
@@ -4,47 +4,24 @@ Contest: Codeforces Educational Round 165 Div. 2
 Link: https://codeforces.com/contest/1969
 Problem: B
 Topic: constructive algorithms, greedy
-Elo: 800
+Elo: N/A
 '''
-def f(arr, size, a, b, cost): # First iteration is (Whole array, 1, l+1 aka 2, cost 0)
-    # print(f'size: {size}, l: {a}, r: {b}, cost: {cost}')
-    print(arr)
-    while a != size:
-        # print(f'l: {a} thing at pos: {arr[a-1]}')
-        if arr[a-1] == 1:
-            break
-        else:
-            a+=1
-    # print(f'l: {l} vs length: {len(binary)}')
-    if a == size:
-        # print("Overflow at L") # return cost here
-        print(cost)
-        return
-    b = a + 1
-    while b != size:
-        print(f'r: {b} thing at pos r-1: {arr[b-1]}')
-        if arr[b-1] == 0:
-            break
-        else:
-            b+=1
-    if b == size:
-        print("Overflow at R")
-        print(cost)
-        return
-    cost += b-a+1
-    arr[a-1] = 0
-    arr[b-1] = 1
-    # arr[l],arr[r] = arr[r],arr[l] # SWAP OPERATION
-    f(arr,len(arr),a+1,b+1,cost)
-
 def solve():
-    binary = [int(i) for i in input()]
-    l = 1
-    r = l+1 
-    cost = 0
-    size = len(binary)
-    f(binary,size,l,r,cost)
-
+    s = input()
+    s = [int(i) for i in s]
+    z = s.count(0)
+    memo = [0,0]
+    ans = 0
+    for c in s:
+        memo[c]+=1
+        if c == 0:
+            ans+= 1 if memo[1] > 0 else 0
+        else:
+            ans+= z - memo[0]
+    print(ans)
 
 if __name__ == "__main__":
-    solve()
+    t = int(input())
+    while t:
+        t-=1
+        solve()
