a9d095a6ecc453093266ba95dcb17e7e5478466d
xMakuno <mamv2001@gmail.com>
2024-05-02 09:56:24 -0600

    📝 add: comments to bin search

--- algorithms/binarysearch.py
+++ algorithms/binarysearch.py
@@ -6,51 +6,39 @@ def bs2(a,x):
         return i
     else:
         return -1
-    
+#Recursive
 def bs1(arr, low, high, x):
- 
     # Check base case
     if high >= low:
- 
         mid = (high + low) // 2
- 
         # If element is present at the middle itself
         if arr[mid] == x:
             return mid
- 
         # If element is smaller than mid, then it can only
         # be present in left subarray
         elif arr[mid] > x:
             return bs1(arr, low, mid - 1, x)
- 
         # Else the element can only be present in right subarray
         else:
             return bs1(arr, mid + 1, high, x)
- 
     else:
         # Element is not present in the array
         return -1
-
+#Iterative
 def bs2(arr, x):
     low = 0
     high = len(arr) - 1
     mid = 0
- 
     while low <= high:
- 
         mid = (high + low) // 2
- 
         # If x is greater, ignore left half
         if arr[mid] < x:
             low = mid + 1
- 
         # If x is smaller, ignore right half
         elif arr[mid] > x:
             high = mid - 1
- 
         # means x is present at mid
         else:
             return mid
- 
     # If we reach here, then the element was not present
     return -1
