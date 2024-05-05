2f64783c411fcdb6d56e0572b26818574e7794e8
xMakuno <mamv2001@gmail.com>
2024-05-05 16:59:49 -0600

    👍add: constructive algorithms practice pt.2

---
+++ Codeforces/contests/div3/943/E.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int t = 3, x, y;
+    
+    while(t--){
+        cin >> x >> y;
+        
+    }
+    return 0;
+}
