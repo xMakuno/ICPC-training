3de06fa5b00fa4e7ec5ec46350aa7e49834b85a4
xMakuno <mamv2001@gmail.com>
2024-05-05 16:59:27 -0600

    👍add: constructive algorithms practice

---
+++ Codeforces/contests/div2/105/A.py
@@ -0,0 +1,16 @@
+'''
+Site: Codeforces
+Contest: Codeforces Round 105 Div 2
+Link: https://codeforces.com/contest/148
+Problem: A
+Topic: constructive algorithms, implementation, math
+Elo: 800
+'''
+if __name__ == "__main__":
+    k = int(input())
+    l = int(input())
+    m = int(input())
+    n = int(input())
+    d = int(input())
+    # Calculate the amount of numbers that meet 1, 2, 3 or 4 criteria then
+    # print the result of N1 - N2 + N3 - N4
