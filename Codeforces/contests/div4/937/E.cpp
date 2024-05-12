b8b8109dd91803f9ce13d20de23b520bb2a3f4e9
xMakuno <mamv2001@gmail.com>
2024-05-11 19:24:55 -0600

    👍add: files for ICPC mexico virtual may 11th

---
+++ Codeforces/contests/div4/937/E.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for(int i = 1; i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+            int satisfy = 2;
+            for(int j = 0; j < i; j++)
+            {
+                for(int k = j+i; k < n; k+=i)
+                {
+                    if(s[k] != s[j])
+                    {
+                        satisfy--;
+                    }
+                }
+            }
+            if(satisfy > 0)
+            {
+                cout << i << endl;
+                return;
+            }
+            satisfy = 2;
+            for(int j = n-i; j < n; j++)
+            {
+                for(int k = j-i; k >= 0; k-=i)
+                {
+                    if(s[k] != s[j])
+                    {
+                        satisfy--;
+                    }
+                }
+            }
+            if(satisfy > 0)
+            {
+                cout << i << endl;
+                return;
+            }
+        }
+    }
+}
+
+int32_t main(){
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ icpc/mexico/mayo11/B.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Bacteria{
+    
+    bool mature;
+};
+int main(){
+    int t = 0;
+    return 0;
+}
