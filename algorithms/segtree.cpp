6470efdb7ba007e4e4139a11308ebf48d491d3f0
xMakuno <mamv2001@gmail.com>
2024-05-11 19:24:22 -0600

    👍add: partial construction of seg tree

---
+++ algorithms/segtree.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void constructTree(int input[], int segTree[], int lo, int hi, int pos){
+    if(lo == hi){
+        segTree[pos] = input[lo];
+        return;
+    }
+    int mid = (lo + hi) / 2;
+}
+
+int main(){
+    return 0;
+}
