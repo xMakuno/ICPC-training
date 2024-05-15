5e7a02061da7942158d052a6918eb5717364b11b
xMakuno <mamv2001@gmail.com>
2024-05-15 11:41:51 -0600

    👎add: WA for Weighted icpc may 4

--- RPC/2024/04/Weighted.cpp
+++ RPC/2024/04/Weighted.cpp
@@ -8,10 +8,26 @@ int main(){
     for(int i = 0; i < n; ++i){
         cin >> nums[i];
     }
+    deque<int> window;
+    priority_queue<pair<int, int>> answer;
+    int index = 1;
     while(!nums.empty()){
         int temp;
         temp = nums.front(); nums.pop_front();
-        
+        window.push_back(temp);
+        if(window.size() >= k){
+            if(window.size() > k){
+                window.pop_front();
+            }
+            int dotp = facts[0]*window[0] + facts[1]*window[1] + facts[2]*window[2];
+            answer.push({-dotp, -index});
+            index++;
+        }
+    }
+
+    for(int i = 0; i < n - k + 1; ++i){
+        cout << -answer.top().second << " " << -answer.top().first << '\n';
+        answer.pop();
     }
     return 0;
 }
