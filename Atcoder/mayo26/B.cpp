8c91b223f68f68e46e87fc8e3ab9051b989612dc
xMakuno <mamv2001@gmail.com>
2024-05-27 21:14:19 -0600

    👍add: potential answer for weekly contest

---
+++ Atcoder/mayo26/B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    string S;
+    cin >> n;
+    if(n & 1 == 1){
+        cout << "-1\n";
+    }else{
+        
+    }
+    return 0;
+}
