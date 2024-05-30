182c38a7553ff433b0dd8ca5578319a398a09660
xMakuno <mamv2001@gmail.com>
2024-05-29 23:22:39 -0600

    👎add: mia problem kattis from uca training

---
+++ UCA/A.cpp
@@ -0,0 +1,50 @@
+/*
+MIA
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+bool dub(int n){
+    switch(n){
+        case 11:
+        case 22:
+        case 33:
+        case 44:
+        case 55:
+        case 66:
+            return true;
+            break;
+        default:
+            return false;
+            break;
+    }
+}
+int main(){
+    int num1[2];
+    cin >> a >> b >> c >> d;
+    string first = ;
+    string second = ;
+    int n = stoi(first);
+    int m = stoi(second);
+    bool x = true, y = true; // true == double
+    if(a != b){
+        x = false;
+    }
+
+    if(c != d){
+        y = false;
+    }
+
+    if(x && y){
+        if(n > m){
+            cout << "Player 1 wins. \n";
+        }else if(n < m){
+            cout << "Player 2 wins. \n";
+        }else{
+            cout << "Tie.\n";
+        }
+    }else{
+
+    }
+    return 0;
+}
