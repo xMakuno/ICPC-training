b68a89a2cbc05a16f75d5881effa301e2bbc8e15
xMakuno <mamv2001@gmail.com>
2024-05-30 20:49:52 -0600

    👎add: temp code A round 786

---
+++ Codeforces/contests/div3/786/B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+int main(){
+    int t;
+    // cin >> t;
+    vector<string> dict;
+    for(int i = 0; i < 26;++i){
+        string temp;
+        temp = alphabet[i];
+        for(){
+
+        }
+    }
+    return 0;
+}
