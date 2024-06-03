460b089df0f7dd8966884ce25c2b75533f8e489a
xMakuno <mamv2001@gmail.com>
2024-06-03 13:51:46 -0600

    🚧add: incomplete codefor USACO speeding ticket

--- USACO/bronze/simulation/speeding-ticket.cpp
+++ USACO/bronze/simulation/speeding-ticket.cpp
@@ -1,37 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    typedef pair<int,int> pii;
-    int n,m;
+    vector<int> distances(101);
+    int n, m;
     cin >> n >> m;
-    vector<pii> road_segments;
-    int carry = 0;
-    for(int i=0; i<n;++i){
+    int pos = 1, journey = 1;
+    for(int i = 0; i < n; ++i){
         int d, v;
         cin >> d >> v;
-        carry += d;
-        road_segments.push_back({carry, v});
+        for(j = journey; i < )
     }
-    for(pii r : road_segments){
-        cout << "distance: " << r.first << " velocity: " << r.second << '\n';
-    }
-    int index = 0, maxDiff = INT_MIN, journey = 0;
-    while(index < n){
-        int d,v;
-        cin >> d >> v;
-        journey += d;
-        
-        if(road_segments[index].first < journey){
-            cout << "distance - " << road_segments[index].first << " journey - "
-            cout << "next\n";
-            index++;
-        }
-
-        if(road_segments[index].second < v){
-            cout << maxDiff << " vs " << road_segments[index].second - v << endl;
-            maxDiff = max(maxDiff, road_segments[index].second - v);
-        }
-    }
-    cout << maxDiff << '\n';
     return 0;
 }
