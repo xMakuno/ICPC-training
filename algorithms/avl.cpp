92d13c3a1aae5c72bd564cda4bdfe7cc3c3a411f
xMakuno <mamv2001@gmail.com>
2024-06-03 13:51:13 -0600

    🚧add: AVL data structure base template

--- algorithms/avl.cpp
+++ algorithms/avl.cpp
@@ -6,7 +6,11 @@ struct Node{
     float x, y;
     Node *left, *right;
     int getHeight(){
+        if(!height) return 0;
         return height;
+    }
+    int getBalance(){
+        return left->getHeight() - right->getHeight();
     }
     void print(){
         cout << "\n==========\n";
@@ -15,10 +19,103 @@ struct Node{
         cout << "\nCoords: ";
         cout << "\nx: " << x;
         cout << "\ny: " << y;
+        cout << "\nheight: " << (!height)? 0: height;
     }
 };
 
 
+/*
+        A
+       / \
+      /   Ar
+     /
+    B
+   / \
+  /   Br
+ /
+C
+
+
+    B
+   / \
+  /   \
+ /     \
+C       A
+       / \
+      /   \
+     Br    Ar
+*/
+
+Node* rightRotation(Node* A){
+    // Extraer 
+    Node* B = A->left;
+    Node* Br = B->right;
+    B->right = A;
+    A-> left = Br;
+
+    A->height = max(A->left->getHeight(), A->right->getHeight()) + 1;
+    B->height = max(B->left->getHeight(), B->right->getHeight()) + 1;
+
+    return B;
+}
+
+/*
+Rotacion 
+Antes:
+
+  A
+ / \
+Al  \
+     \
+      B
+     / \
+    Bl  \
+         \
+          C
+
+Despues:
+        B
+       / \
+      /   \
+     /     \
+    A       C
+   / \
+  Al  Bl
+
+*/
+
+Node* leftRotation(Node* A){
+    Node* B = A->right;
+    Node* Bl = B->left;
+
+    B->left = A;
+    A->right = Bl;
+
+    A->height = max(A->left->getHeight(), A->right->getHieght()) + 1;
+    B->height = max(B->left->getHeight(), B->right->getHieght()) + 1;
+
+    return B;
+}
+
+Node* insertNode(){
+}
+
+Node* search(Node* r, int ){
+}
+
+void showPre(Node* head){
+    if(head != NULL){
+        cout << head->id << " ";
+        showPre(head->left);
+        showPre(head->right);
+    }
+}
+void showIn(){
+
+}
+void showPos(){
+
+}
 
 int main(){
     Node* tree;
