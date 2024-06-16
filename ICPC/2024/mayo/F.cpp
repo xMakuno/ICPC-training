5e6998d10017a4bcfcb0f06d2d01df7235fa5a02
xMakuno <mamv2001@gmail.com>
2024-06-16 12:56:36 -0600

    👎add: incomplete code for F

--- ICPC/2024/mayo/F.cpp
+++ ICPC/2024/mayo/F.cpp
@@ -3,18 +3,16 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<pair<int,int>> coords(n,0);
+    vector<pair<int,int>> coords(n,{0,0});
     for(int i = 0; i < n;++i){
-        cin >> coords[i];
+        cin >> coords[i].first;
     }
     // O(n^3) : max n => 15 : 15^3 => 3375 iterations
     for(int i = 0; i < n; ++i){
         for(int j = i+1;j<n; ++j){
             for(int k = j+1; k<n;++k){
                 //Probs O(1)
-                /*
-                Group subsets by value of area
-                */
+                //Group of subsets by value of area
             }
         }
     }
