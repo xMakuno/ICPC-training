51094bfcb597ac9eef14afe65ee643d1334535dc
xMakuno <mamv2001@gmail.com>
2024-06-23 10:29:00 -0600

    :art: update: project structure

---
+++ ICPC/2024/junio/G.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<set<int>> &graph, vector<bool> &v,  int target){
+	if(v[target]) return;
+	v[target] = true;
+	set<int> neighbors = graph[target];
+	for(const int &n : neighbors){
+		dfs(graph, v, n);
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+	//Since there is at most 1 edge between node u and v
+	vector<set<int>> neighbor(n+1);
+	// Begin Adjancency List
+	int u, v;
+	for(int i = 1; i < n; ++i){
+		cin >> u >> v;
+		neighbor[u].insert(v);
+		neighbor[v].insert(u);	
+	}
+	/*
+	 * Print out Adjacency List 
+	 * for(int i = 1; i <= n; ++i){
+		cout << "\nNode " << i << " -> ";
+		for(const int &v: neighbor[i]){
+			cout << v << " ";
+		}
+	}*/
+	// Visited Vector
+	vector<bool> visited(n+1,0);
+	// Start node 1
+	dfs(neighbor, visited, 1);
+	for(int i = 1; i < n; ++i){
+		cout << "\nNode " << i << " is set to: " << visited[i];
+	}
+	return 0;
+}
---
+++ ICPC/2024/junio/test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+	int u, v;
+	vector<tuple<int, int, int>> E;
+	// Start receiving edges
+	for(int i = 0; i < N-1; ++i){
+		cin >> u >> v;
+		if( u > v) swap(u,v);
+		E.push_back({u, v, 1});
+	}
+	cout << "Edges:\n";
+	for(int i = 0; i < N-1; ++i){
+		cout << "Start: " << get<0>(E[i]);
+		cout << " - End: " << get<1>(E[i]);
+		cout << '\n';
+	}
+
+	return 0;
+}
