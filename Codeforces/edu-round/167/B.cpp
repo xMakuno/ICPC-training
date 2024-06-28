76ab00f202d56bfe8b27f9d22268079f397b7c89
xMakuno <mamv2001@gmail.com>
2024-06-28 12:18:30 -0600

    👍add: AC for B edu 167

--- Codeforces/edu-round/167/B.cpp
+++ Codeforces/edu-round/167/B.cpp
@@ -8,9 +8,22 @@ int main(){
     while(T--){
         string a, b;
         cin >> a >> b;
-        for(int i=0; i < a.size();++i){
-            
+        int ans = a.size() + b.size();
+        int start = 0, max_shared = -1;
+        while(start < b.size()){
+            int shared = 0;
+            int ptr = start;
+            for(int i = 0; i < a.size(); ++i){
+                if(a[i] == b[ptr]){
+                    ptr++;
+                    shared++;
+                }
+            }
+            max_shared = max(max_shared, shared);
+            start++;
         }
+        
+        cout << ans - max_shared << '\n';
     }
     return 0;
 }
