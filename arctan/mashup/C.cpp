23cfec58426e838a6a14e33af94087895f80fb81
xMakuno <mamv2001@gmail.com>
2024-06-28 23:05:32 -0600

    👍add: AC for arctan mashup

--- arctan/mashup/C.cpp
+++ arctan/mashup/C.cpp
@@ -7,19 +7,21 @@ int main(){
     while(T--){
         int n,temp; 
         cin >> n;
-        multiset<int> m;
+        vector<int> m;
         int mn = INT_MAX, mx = -1;
         for(int i = 0; i < n; ++i){
             cin >> temp;
             mn = min(mn, temp);
             mx = max(mx, temp);
-            m.insert(temp);
+            m.push_back(temp);
         }
-        if(m.size() <= 2){
-            cout << "0\n";
-        }else{
-            cout << n - m.count(mn) - m.count(mx) << '\n';
+        int ans = n;
+        for(int &x: m){
+            if(x == mn || x == mx){
+                ans--;
+            }
         }
+        cout << ans << '\n';
     }
     return 0;
 }
--- arctan/mashup/D.cpp
+++ arctan/mashup/D.cpp
@@ -1,16 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define YES cout << "YES\n";
+#define NO cout << "NO\n";
 typedef long long ll;
 int main(){
     int T = 1;
     cin >> T;
     while(T--){
-        int n, d;
+        ll n, d;
         cin >> n >> d;
-        int x = floor(sqrt(d)-1), y = ceil(sqrt(d)-1);
-        if(n == d){
-            
+        ll x = (n-1)/2;
+        if( n >= (x+ (d+x)/(x+1)) ){
+            YES
+        }else{
+            NO
         }
+
     }
     return 0;
 }
