8751fc75973f3c309a66a662dd56d59593d48c51
xMakuno <mamv2001@gmail.com>
2024-07-02 11:09:59 -0600

    👍add: not really AC for number spiral CSES

--- CSES/Introductory/NumberSpiral.cpp
+++ CSES/Introductory/NumberSpiral.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 typedef long long ll;
 
-
 int main(){
-    int T = 1;
+    ll T = 1;
     cin >> T;
     while(T--){
-
+        ll y,x;
+        cin >> y >> x;
+        ll mx_i = max(x,y);
+        if(y > x){
+            if(y & 1 == 1){
+                cout << (mx_i-1) * (mx_i-1) + x << '\n';
+            }else{
+                cout << (mx_i-1) * (mx_i-1) + (2*y-x) << '\n';
+            }
+        }else{
+            if(x & 1 == 0){
+                cout << (mx_i-1) * (mx_i-1) + y << '\n';
+            }else{
+                cout << (mx_i-1) * (mx_i-1) + (2*x-y) << '\n';
+            }
+        }
     }
     return 0;
 }
