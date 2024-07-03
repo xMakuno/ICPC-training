232e9cc6440b373ca72957fb0f8bfe4089991414
xMakuno <mamv2001@gmail.com>
2024-07-02 23:18:50 -0600

    👎add: WA for B abc 360

--- Atcoder/abc/360/B.cpp
+++ Atcoder/abc/360/B.cpp
@@ -4,5 +4,27 @@ typedef long long ll;
 int main(){
     string S, T;
     cin >> S >> T;
+    if(T.size() == 1){
+        for(int i = 0; i < S.size()-1; ++i){
+            if(S[i] == T[0]){
+                cout << "Yes\n";
+                return 0;
+            }
+        }
+        cout << "No\n";
+        return 0;
+    }
+    int limit = T.size()-1;
+    vector<string> lanes(limit, "");
+    for(int i = 0; i < S.size(); ++i){
+        lanes[i%limit] += S[i];
+    }
+    for(string s: lanes){
+        if(s == T){
+            cout << "Yes\n";
+            return 0;
+        }
+    }
+    cout << "No\n";
     return 0;
 }
