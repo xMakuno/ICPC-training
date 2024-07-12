8aa20c0f52868e90f5ab6d8b773eb6d98593405d
xMakuno <mamv2001@gmail.com>
2024-07-11 21:01:33 -0600

    👍add: AC for NumberSpiral CSES

--- CSES/Introductory/NumberSpiral.cpp
+++ CSES/Introductory/NumberSpiral.cpp
@@ -10,13 +10,13 @@ int main(){
         cin >> y >> x;
         ll mx_i = max(x,y);
         if(y > x){
-            if(y & 1 == 1){
+            if((y & 1) == 1){
                 cout << (mx_i-1) * (mx_i-1) + x << '\n';
             }else{
                 cout << (mx_i-1) * (mx_i-1) + (2*y-x) << '\n';
             }
         }else{
-            if(x & 1 == 0){
+            if((x & 1) == 0){
                 cout << (mx_i-1) * (mx_i-1) + y << '\n';
             }else{
                 cout << (mx_i-1) * (mx_i-1) + (2*x-y) << '\n';
