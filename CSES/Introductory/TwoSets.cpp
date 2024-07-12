c28d1f145ace9c27c46f3a00438fb6da6ba409ac
xMakuno <mamv2001@gmail.com>
2024-07-11 22:24:51 -0600

    👍add: AC for TwoSets CSES

--- CSES/Introductory/TwoSets.cpp
+++ CSES/Introductory/TwoSets.cpp
@@ -9,18 +9,38 @@ int main(){
         cout << "NO\n";
         return 0;
     }
-    set<int, greater<int>> all_nums;
-    for(int i = 1; i <= n;++i){
-        all_nums.insert(i);
+    cout << "YES\n";
+    vector<int> perm(n);
+    for(int i = 0; i < n; ++i){
+        perm[i] = i+1;
+    
     }
-    int groups = n / 2;
-    set<int> ans1, ans2;
-    while(!all_nums.empty()){
-        if(ans1.size() < groups){
-            
+    int l = 0, r = perm.size() - 1;
+    vector<int> v1, v2;
+    if(n&1){
+        v1.push_back(perm[r]);
+        r--;
+    }
+    bool turnTwo = true;
+    while(l < r){
+        if(turnTwo){
+            v2.push_back(perm[r]);
+            v2.push_back(perm[l]);
         }else{
-
+            v1.push_back(perm[r]);
+            v1.push_back(perm[l]);
         }
+        l++;
+        r--;
+        turnTwo = !turnTwo;
+    }
+    cout << v1.size() << '\n';
+    for(int i = 0; i < v1.size(); ++i){
+        cout << v1[i]<< " \n"[i==v1.size()-1];
+    }
+    cout << v2.size() << '\n';
+    for(int i = 0; i < v2.size(); ++i){
+        cout << v2[i] << " \n"[i==v2.size()-1];
     }
     return 0;
 }
