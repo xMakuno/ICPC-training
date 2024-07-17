84d403df13509e3401c85bd1a0f76c6de24d8c74
xMakuno <mamv2001@gmail.com>
2024-07-16 20:30:26 -0600

    👍add: AC for div2 803

---
+++ Codeforces/contests/div2/803/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    int T = 1;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        int ans;
+        for(int i = 0; i < n; ++i){
+            cin >> ans;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
