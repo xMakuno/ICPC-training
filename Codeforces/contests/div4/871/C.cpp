1927c65df3389d919eebdbdeb445ff1d80fe434b
xMakuno <mamv2001@gmail.com>
2024-07-18 16:11:38 -0600

    👍add: AC for div 4 871

--- Codeforces/contests/div4/871/C.cpp
+++ Codeforces/contests/div4/871/C.cpp
@@ -16,6 +16,7 @@ int main(){
         int n;
         cin >> n;
         int s1 = 1e9+5, s2 = 1e9+5, s3 = 1e9+5;
+        int know = 0;
         for(int i=0; i <n; ++i){
             int a;
             string b;
@@ -25,20 +26,28 @@ int main(){
             if(task){
                 switch(task){
                     case 1:
+                        know = SET(know,0);
                         s1 = min(s1,a);
                         break;
                     case 2:
+                        know = SET(know,1);
                         s2 = min(s2,a);
                         break;
                     case 3:
+                        know = SET(know,0);
+                        know = SET(know,1);
                         s3 = min(s3,a);
                         break;
                         
                 }
             }
         }
-        // FIXME: does not print -1
-        cout << (s1+s2 < s3 ? s1+s2 :s3) << '\n';
+        if(know != 3){
+            cout << "-1\n";
+        }else{
+            // FIXME: does not print -1
+            cout << (s1+s2 < s3 ? s1+s2 :s3) << '\n';
+        }
     }
     return 0;
 }
