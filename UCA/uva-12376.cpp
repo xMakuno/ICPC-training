f4a4ef382fdf269200ac0fea09550f81146e7278
xMakuno <mamv2001@gmail.com>
2024-10-02 22:51:36 -0600

    👍add: AC for uca 12376

--- UCA/uva-12376.cpp
+++ UCA/uva-12376.cpp
@@ -3,45 +3,42 @@
 using namespace std;
 typedef long long ll;
 
+pair<int, int> dfs(vector<bool> &visited, vector<vector<int>> &al, vector<int> &vals, int u, int sum){
+    // cout << "Looking: " << u << '\n';
+    visited[u] = true;
+    int option = 0;
+    for(auto &v: al[u]){
+        
+        if(visited[v] == false && vals[option] < vals[v]){
+            option = v;
+        }
+    }
+    // cout << option << '\n';
+    if(option == 0){
+        return make_pair(u, sum);
+    }
+
+    return dfs(visited, al, vals, option, sum + vals[option]);
+}
+
 int main(){
     int T; cin >> T;
     for(int t = 1; t <= T; ++t){
-        //solve();
         int n, m;
-        // 6 6
         cin >> n >> m;
-        vi values(n,0);
-        // 0 8 9 2 7 5
-        for(int i = 0; i <n; ++i){
+        vector<int> values(n);
+        for(int i = 0; i < n; ++i){
             cin >> values[i];
         }
-        vector<vector<int>> AL(m);
+        vector<vector<int>> AL(n);
+        int u, v;
         for(int i = 0; i < m; ++i){
-            int u, v;
             cin >> u >> v;
-            // al @ 5 -> 4, and so on
             AL[u].push_back(v);
         }
-        // ya procese todo
-        int ans = 0, option;
-        queue<int> q; q.push(0);
-        while(!q.empty()){
-            int u = q.front(); q.pop();
-            int greed = -1;
-            for(int &v : AL[u]){
-                if(greed < values[v]){
-                    greed = values[v];
-                    option = v;
-                }
-            }
-            ans += greed;
-            // si llega a una "leaf"
-            if(AL[option].size() > 0){
-                q.push(option);
-            }
-        }
-        // printf("Case %d: %d %d\n", t, ans, option);
-        cout << "Case " << t << ": " << ans << " " << option << '\n';
+        vector<bool> visited(n, false);
+        pair<int, int> ans = dfs(visited, AL, values, 0, 0);
+        printf("Case %lld: %lld %lld\n", t, ans.second, ans.first);
     }
     return 0;
 }
