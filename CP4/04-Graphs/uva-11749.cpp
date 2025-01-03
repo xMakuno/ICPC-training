716afffa58e24bd5a17546769634f9bb7ffc89fb
xMakuno <mamv2001@gmail.com>
2025-01-02 19:26:02 -0600

    add: partial code for ucva 11749

---
+++ CP4/04-Graphs/uva-11749.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+    int N, M;
+    while(true){
+        cin >> N >> M;
+        if(N == 0 && M == 0){
+            break;
+        }
+    }
+    return 0;
+}
