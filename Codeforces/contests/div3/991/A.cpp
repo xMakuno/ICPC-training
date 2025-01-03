2e72f22d3af073ab513cc1b9a542856da7588203
xMakuno <mamv2001@gmail.com>
2025-01-02 19:26:31 -0600

    add: partial code A round 991 div3

---
+++ Codeforces/contests/div3/991/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    while(T--){
+        int n, m;
+        cin >> n >> m;
+        int counts[11];
+        for(int i = 0; i < n; ++i){
+            string S;
+            cin >> S;
+            
+        }
+
+    }
+    return 0;
+}
