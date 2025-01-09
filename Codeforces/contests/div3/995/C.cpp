f8a5d01b30dd9ab9b0065980a246256d472cf053
xMakuno <mamv2001@gmail.com>
2025-01-08 23:24:12 -0600

    👎add: bruh for C div3 r995

--- Codeforces/contests/div3/995/C.cpp
+++ Codeforces/contests/div3/995/C.cpp
@@ -10,17 +10,44 @@ int main(){
         for(int i = 0; i < m; ++i){
             cin >> lists[i];
         }
-        set<int> mex;
-        for(int i = 1; i <= n; ++i){
-            mex.insert(i);
+        if(n-k == 0){
+            for(int i = 0; i < k; ++i){
+                int temp;
+                cin >> temp;
+            }
+            for(int i = 0; i < m; ++i){
+                cout << "1";
+            }
+            cout << '\n';
+        }else if(n-k > 1){
+            for(int i = 0; i < k; ++i){
+                int temp;
+                cin >> temp;
+            }
+            for(int i = 0; i < m; ++i){
+                cout << "0";
+            }
+            cout << '\n';
+        }else{
+            set<int> mex;
+            for(int i = 1; i <= n; ++i){
+                mex.insert(i);
+            }
+            for(int i = 0; i < k; ++i){
+                int temp;
+                cin >> temp;
+                mex.erase(temp);
+            }
+            int M = *(mex.begin()++);
+            for(int i = 0; i < m; ++i){
+                if(i+1 == M){
+                    cout << "1";
+                }else{
+                    cout << "0";
+                }
+            }
+            cout << '\n';
         }
-        for(int i = 0; i < k; ++i){
-            int temp;
-            cin >> temp;
-            mex.erase(temp);
-        }
-        int mex = *(mex.begin()++);
-        
     }
     return 0;
 }
