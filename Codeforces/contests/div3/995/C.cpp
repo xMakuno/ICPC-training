9bc45aa7f8f1bde4a7c4bfa5f7974fa7d6204d7f
xMakuno <mamv2001@gmail.com>
2025-01-14 23:48:14 -0600

    thing

--- Codeforces/contests/div3/995/C.cpp
+++ Codeforces/contests/div3/995/C.cpp
@@ -10,43 +10,20 @@ int main(){
         for(int i = 0; i < m; ++i){
             cin >> lists[i];
         }
+        bool used[m+1];
+        memset(used, false, m+1);
         if(n-k == 0){
-            for(int i = 0; i < k; ++i){
-                int temp;
-                cin >> temp;
-            }
             for(int i = 0; i < m; ++i){
                 cout << "1";
             }
             cout << '\n';
         }else if(n-k > 1){
-            for(int i = 0; i < k; ++i){
-                int temp;
-                cin >> temp;
-            }
             for(int i = 0; i < m; ++i){
                 cout << "0";
             }
             cout << '\n';
         }else{
-            set<int> mex;
-            for(int i = 1; i <= n; ++i){
-                mex.insert(i);
-            }
-            for(int i = 0; i < k; ++i){
-                int temp;
-                cin >> temp;
-                mex.erase(temp);
-            }
-            int M = *(mex.begin()++);
-            for(int i = 0; i < m; ++i){
-                if(i+1 == M){
-                    cout << "1";
-                }else{
-                    cout << "0";
-                }
-            }
-            cout << '\n';
+            
         }
     }
     return 0;
